095f6ce003f883e456fdc6e1fc8f8092d1ef7f4e
tybeller <ty.c.beller@gmail.com>
2024-06-07 02:01:53 -0400

    complete with pictures no video

--- embed.py
+++ embed.py
@@ -1,26 +1,55 @@
 import discord
+from datetime import datetime, timezone, timedelta
+
+def create_leaderboard_embed(leaderboard_str):
+    embed = discord.Embed(
+        title = "🏆 **Leaderboard** 🏆",
+        color=discord.Color.teal(),
+        description=leaderboard_str
+    )
+    return embed
 
 def create_embed(message, fire_count, rank):
-    title = "🔥🔥🔥 MOST FYE 🔥🔥🔥"
+    
+    place_ending = lambda n: str(n)+'tsnrhtdd'[n%5*(n%100^15>4>n%10)::4] #https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd?page=1&tab=scoredesc#tab-top
+    title = f'{place_ending(rank)} '
     color = discord.Color.yellow()
+
     if rank == 2:
-        title = "🔥🔥 SECOND MOST FYE 🔥🔥"
+        title += "🥈"
         color = discord.Color.blue()
     elif rank == 3:
-        title = "🔥 THIRD MOST FYE 🔥"
+        title += "🥉"
         color = discord.Color.red()
+    else: 
+        title += "🥇"
+    title += ""
+
+    description = None
+
+    if message.content.strip() != "":
+        description = "# " + message.content
     
     embed = discord.Embed(
         title = title,
         color = color,
-        description = message.content
+        description = description
     )
-    
-    embed.add_field(name="🔥 Reacts", value=fire_count, inline=True)
-    embed.add_field(name="Date", value=message.created_at.strftime("%m/%d/%Y, %H:%M:%S"), inline=True)
+    est = timezone(timedelta(hours=-5))
+
+    message_datetime_est = message.created_at.astimezone(est)
+
+    time_str = message_datetime_est.strftime("%I:%M %p")
+    date_str = message_datetime_est.strftime("%m/%d/%y")
+
+    embed.add_field(name="Time", value=time_str, inline=True)
+    embed.add_field(name="Date", value=date_str, inline=True)
+    embed.add_field(name="Link", value=f'[Jump To]({message.jump_url})', inline=True)
+    embed.add_field(name="🔥 Reacts", value=fire_count, inline=False)
+
     
     embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
-    embed.set_thumbnail(url=message.author.display_avatar.url)
+    #embed.set_thumbnail(url=message.author.display_avatar.url)
     
     if message.attachments:
         embed.set_image(url=message.attachments[0].url)
--- fire.py
+++ fire.py
@@ -3,7 +3,8 @@ from discord import app_commands
 from discord.ext import commands
 import datetime 
 from datetime import timezone, datetime, timedelta
-from embed import create_embed
+from collections import defaultdict
+from embed import create_embed, create_leaderboard_embed
 from heapq import heapify, heappush, heappop
 from dotenv import load_dotenv
 import logging
@@ -43,34 +44,24 @@ async def sync(ctx):
     description="Posts a leaderboard of the top fire reacted messages in the past week"
 )
 async def fire_weekly(interaction):
+    if not interaction.user.id == admin_id:
+        print("User ", ctx.author.display_name, " attempted to invoke weekly post")
+        return
     try:
-        print("start weekly")
+        print("fire weekly")
         channel = bot.get_channel(interaction.channel_id)
-        current_datetime_utc = datetime.now(timezone.utc)
-        one_week_ago_datetime_utc = current_datetime_utc - timedelta(weeks=1)
         
-        #min heap. will take in tuples of (fire_count, message).
-        message_min_heap = []
-        heapify(message_min_heap) 
+        print("awaiting")
+        embeds = await build_embeds(interaction, channel)
+        print("embeds complete")
+       
+        message = """
+        # 🔥 WEEKLY FIRE REACT ROUNDUP 🔥
+        ## Who got the most fire reacts this past week
+        """
+        print("message")
 
-        async for message in channel.history(before=current_datetime_utc, after=one_week_ago_datetime_utc):
-            for reaction in message.reactions:
-                if reaction.emoji == "🔥":
-                    #when we have a message with fire reacts, push it into the heap sorted by react count
-                    heappush(message_min_heap, (reaction.count, message.id))
-                    #if the heap is greater than the leaderboard_size, remove the message with the least fire reacts.
-                    if len(message_min_heap) > leaderboard_size:
-                        heappop(message_min_heap)
-
-        #now our heap contains the messages with the most fire reacts in the past week.
-        embeds = []
-        while not len(message_min_heap) == 0:
-            fire_count, message_id = heappop(message_min_heap)
-
-            message = await channel.fetch_message(message_id)
-            embeds.insert(0, create_embed(message, fire_count, len(message_min_heap)+1))
-        
-        await channel.send(embeds=embeds)
+        await channel.send(content=message, embeds=embeds)
 
 
     except discord.NotFound:
@@ -83,6 +74,60 @@ async def fire_weekly(interaction):
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+
+async def build_embeds(interaction, channel):
+    print("starting build embeds")
+    current_datetime_utc = datetime.now(timezone.utc)
+    one_week_ago_datetime_utc = current_datetime_utc - timedelta(weeks=1)
+    
+    #min heap. will take in tuples of (fire_count, message).
+    message_min_heap = []
+    heapify(message_min_heap) 
+
+    #dictionary stores all fire reactions recieved by a user in the week
+    fire_reacts_per_user = defaultdict(int)
+    
+    print("parsing messages")
+    async for message in channel.history(before=current_datetime_utc, after=one_week_ago_datetime_utc, limit=None):
+        for reaction in message.reactions:
+            if reaction.emoji == "🔥":
+                #when we have a message with fire reacts, push it into the heap sorted by react count
+                heappush(message_min_heap, (reaction.count, message.id))
+                #if the heap is greater than the leaderboard_size, remove the message with the least fire reacts.
+                if len(message_min_heap) > leaderboard_size:
+                    heappop(message_min_heap)
+                
+                #add fire reacts to user's count
+                fire_reacts_per_user[message.author.display_name] += reaction.count
+    
+    #now our heap contains the messages with the most fire reacts in the past week.
+    embeds = []
+    print("creating embeds")
+    while not len(message_min_heap) == 0:
+        fire_count, message_id = heappop(message_min_heap)
+
+        message = await channel.fetch_message(message_id)
+        embeds.insert(0, create_embed(message, fire_count, len(message_min_heap)+1))
+    
+    #sort users by fire reactions
+    leaderboard = ""
+    print("leaderboard")
+    for place, fire_tuple in enumerate(sorted(fire_reacts_per_user.items(), key=lambda item:item[1], reverse=True)):
+        user, fire_count = fire_tuple
+        place_ending = lambda n: str(n)+'tsnrhtdd'[n%5*(n%100^15>4>n%10)::4] #https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd?page=1&tab=scoredesc#tab-top
+
+        prefix = ""
+        if place + 1 == 1:
+            prefix = "# "
+        elif place + 1 == 2:
+            prefix = "## "
+        elif place + 1 == 3:
+            prefix = "### "
+        leaderboard += prefix + f'**{place_ending(place+1)}: {user}** - {fire_count}\n'
+    embeds.insert(0, create_leaderboard_embed(leaderboard))
+    return embeds
+
 
 
 handler = logging.FileHandler(filename='discord,log', encoding='utf-8', mode='w')
